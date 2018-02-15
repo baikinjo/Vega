8d525a445dc9d6e5642f75357520a1af0c87a7c5
Injo Baik <baikinjo.28@gmail.com>
2018-02-15 00:14:41 -0800

    added new resources, migrations, controllers

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.context = context;
+            this.mapper = mapper;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+                   
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+                   
+            var vehicle = await context.Vehicles.FindAsync(id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+    }
+}
